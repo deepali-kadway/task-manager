d6ffb20242e570e60aa5e5f757dc61dd81259147
Deepali Kadway <dkadway@gmail.com>
2025-07-31 17:33:27 -0600

    post api

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Add middleware to parse JSON from request body
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Task Manager API is running');
+})
+
+app.post('/', (req, res) => {
+    const taskId = req.body.id; // Get id from request body
+    res.send(`Task with ID ${taskId} has been created`);
 })
 
 app.listen(3000, () => {
